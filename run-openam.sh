650b4a9fb346e1b2c280eaa74b0b402ad61fc3fa
milo-minderbinder <minderbinder.enterprises@gmail.com>
2015-02-24 11:48:21 -0500

    simplified Docker run script

--- run-openam.sh
+++ run-openam.sh
@@ -6,24 +6,24 @@ read -p "Keystore password:" -s ks_pass
 echo
 read -p "Keystore key alias:" ks_keyalias
 
+TAG=latest
+MODE="-it --rm"
 if [[ $1 == "-d" ]]; then
-	if [[ -z "$2" ]]; then
-		TAG=latest
-	else
+	MODE="-d"
+	if [[ -n "$2" ]]; then
 		TAG="${2}"
 	fi
-	docker run -d -v "${ks_file}":/usr/share/openam/keystore.jks:ro \
-		-p 127.0.0.1:8080:8080 -p 127.0.0.1:8443:8443 -h openam.example.com --name openam \
-		-e KEYSTORE_FILE=/usr/share/openam/keystore.jks -e KEYSTORE_PASS="${ks_pass}" -e KEYSTORE_KEYALIAS="${ks_keyalias}"  \
-		mminderbinder/openam:${TAG}
 else
-	if [[ -z "$1" ]]; then
-		TAG=latest
-	else
+	if [[ -n "$1" ]]; then
 		TAG="${1}"
 	fi
-	docker run -it --rm -v "${ks_file}":/usr/share/openam/keystore.jks:ro \
-		-p 127.0.0.1:8080:8080 -p 127.0.0.1:8443:8443 -h openam.example.com --name openam \
-		-e KEYSTORE_FILE=/usr/share/openam/keystore.jks -e KEYSTORE_PASS="${ks_pass}" -e KEYSTORE_KEYALIAS="${ks_keyalias}"  \
-		mminderbinder/openam:${TAG}
 fi
+docker run ${MODE} \
+	-v "${ks_file}":/usr/share/openam/keystore.jks:ro \
+	-p 127.0.0.1:8080:8080 \
+	-p 127.0.0.1:8443:8443 \
+	-h openam.example.com --name openam \
+	-e KEYSTORE_FILE=/usr/share/openam/keystore.jks \
+	-e KEYSTORE_PASS="${ks_pass}" \
+	-e KEYSTORE_KEYALIAS="${ks_keyalias}"  \
+	mminderbinder/openam:${TAG}
